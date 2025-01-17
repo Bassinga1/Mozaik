7fad95dc438b12c7f2ac19652e4e0b13436c9aae
Bassinga <pribenis@gmail.com>
2025-01-17 01:11:12 +0100

    macros _form de service

--- public/js/add-item-link.js
+++ public/js/add-item-link.js
@@ -1,7 +1,21 @@
 // on copie/colle le script de l'aide : https://symfony.com/doc/current/form/form_collections.html#allowing-tags-to-be-removed
 
+const addImageFormDeleteLink = (item) => {
+    const removeFormButton = document.createElement('button');
+    removeFormButton.innerText = 'Delete this image';
+
+    item.append(removeFormButton);
+
+    removeFormButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        // remove the li for the tag form
+        item.remove();
+    });
+}
+
+
 // On récupère l'ul qui contient la collection de formulaires dynamiques
-function addFormToCollection(e) {
+const addFormToCollection = (e) => {
     const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
     // On crée un li, qui contiendra le prototype du formulaire
     const item = document.createElement('li');
@@ -16,13 +30,21 @@ function addFormToCollection(e) {
         collectionHolder.dataset.index
         );
     // On ajoute un bouton de suppression
+    addImageFormDeleteLink(item);
+    // On ajoute la li au ul
     collectionHolder.appendChild(item);
-
+    // On incrémente l'index de la collection (attribut data-index de l'ul)
     collectionHolder.dataset.index++;
 };
-
+// Prise en charge du clic sur le button d'ajout d'une image
 document
     .querySelectorAll('.add_item_link')
     .forEach(btn => {
         btn.addEventListener("click", addFormToCollection)
 });
+// Mise en plqce des boutons supressions
+document
+    .querySelectorAll('ul.images li')
+    .forEach((item) => {
+        addImageFormDeleteLink(item);
+    });
